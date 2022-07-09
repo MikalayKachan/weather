9977372fd34e9b106969c86b300aebb94ffaf4cf
nik <nik@MacBook-Air-Anton-2.local>
2022-07-09 12:28:21 +0300

    feature/WA-18 Added routings and Error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { CityPage } from 'components/pages/CityPage';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import { HomePage } from 'components/pages/HomePage';
+import { CityPage } from 'components/pages/CityPage';
+import { ErrorPage } from 'components/pages/ErrorPage';
 
 function App() {
   return (
-    <div>
-      <CityPage />;
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/" exact={true} component={HomePage} />
+          <Route path="/:city" exact={true} component={CityPage} />
+          <Route path="/:error" exact={true} component={ErrorPage} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
