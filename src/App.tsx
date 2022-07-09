9eb7af6005c69bf53ef9d1a942bd5c9d607d8748
nik <nik@MacBook-Air-Anton-2.local>
2022-07-09 20:04:27 +0300

    feautre/WA-18 Updated constants for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import { ROUTES } from 'constants/routes';
+
 import { HomePage } from 'components/pages/HomePage';
 import { CityPage } from 'components/pages/CityPage';
 import { ErrorPage } from 'components/pages/ErrorPage';
@@ -8,13 +10,11 @@ import { ErrorPage } from 'components/pages/ErrorPage';
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Switch>
-          <Route path="/" exact={true} component={HomePage} />
-          <Route path="/:city" exact={true} component={CityPage} />
-          <Route path="/:error" exact={true} component={ErrorPage} />
-        </Switch>
-      </div>
+      <Switch>
+        <Route path={ROUTES.ROOT} exact={true} component={HomePage} />
+        <Route path={ROUTES.CITY_PAGE} exact={true} component={CityPage} />
+        <Route path={ROUTES.ERROR_PAGE} exact={true} component={ErrorPage} />
+      </Switch>
     </BrowserRouter>
   );
 }
