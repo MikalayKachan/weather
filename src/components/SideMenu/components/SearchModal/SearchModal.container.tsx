b01979446d7d4a00f11d0120633aa63089cc14e5
Anton Kasheuski <kascheikinen@gmail.com>
2022-07-21 18:22:28 +0300

    feature/WA-12 Added possibility to choose city

--- src/components/SideMenu/components/SearchModal/SearchModal.container.tsx
+++ src/components/SideMenu/components/SearchModal/SearchModal.container.tsx
@@ -22,6 +22,7 @@ type PropsType = {
 
 const SearchModalContainer = ({ isSearchModalOpen, onClose }: PropsType) => {
   const [searchValue, setSearchValue] = useState('');
+  const [cityToSave, setCityToSave] = useState<null | CityType>(null);
 
   let history = useHistory();
 
@@ -54,9 +55,36 @@ const SearchModalContainer = ({ isSearchModalOpen, onClose }: PropsType) => {
 
   const handleCityClick = (id: number) => {
     const nextCity = citiesList.find((city) => city.lat === id);
+    onClose();
     nextCity &&
       history.push('/city?lat=' + nextCity.lat + '&lon=' + nextCity.lon);
   };
+
+  const handleAddCityClick = (id: number) => {
+    const nextCity = citiesList.find((city) => city.lat === id) as CityType;
+    const newCityToSave = {
+      name: nextCity.name,
+      country: nextCity.country,
+      lat: nextCity.lat,
+      lon: nextCity.lon,
+    };
+    nextCity && setCityToSave(newCityToSave);
+  };
+
+  useEffect(() => {
+    const JSONCities = localStorage.getItem('savedCities');
+    const citiesFromStorage = JSONCities && JSON.parse(JSONCities);
+    if (citiesFromStorage === null) {
+      cityToSave &&
+        localStorage.setItem('savedCities', JSON.stringify([cityToSave]));
+    } else {
+      cityToSave &&
+        localStorage.setItem(
+          'savedCities',
+          JSON.stringify([...citiesFromStorage, cityToSave]),
+        );
+    }
+  }, [cityToSave]);
 
   return (
     <SearchModal
@@ -67,6 +95,7 @@ const SearchModalContainer = ({ isSearchModalOpen, onClose }: PropsType) => {
       citiesListLoading={citiesListLoading}
       citiesList={citiesList}
       onCityClick={handleCityClick}
+      onAddCityClick={handleAddCityClick}
     />
   );
 };
